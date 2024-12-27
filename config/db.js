bbe2810ba6624b86a554269448ee5400b64f883c
milan <1234mrego@gmail.com>
2024-12-27 17:38:10 +0530

    git ignore added

--- config/db.js
+++ config/db.js
@@ -5,6 +5,11 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000, 
+        }).then(() => {
+            console.log("MongoDB connected");
+        }).catch((error) => {
+            console.error("MongoDB connection error:", error);
         });
         console.log('MongoDB connected');
     } catch (err) {
